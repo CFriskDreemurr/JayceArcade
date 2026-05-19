ed83320f2cf7a8887261b50623653eebdeb60825
KacperSiak <kacper.siakowski47@gmail.com>
2026-05-19 16:25:04 +0200

    clearing in folders

---
+++ Assets/Scripts/BottleGames/BottleGame.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BottleGame : MonoBehaviour
+{
+    [SerializeField] private Collider kockiKlocki;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
