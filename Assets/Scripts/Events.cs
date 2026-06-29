35c55b0c9ac2d04ee3a0f130170e21e0f806a93c
Kacper Siąkowski <ksiakowski@edu.cdv.pl>
2026-06-30 01:35:11 +0200

    prizewall wokrs

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -4,10 +4,15 @@ using System;
 public class Events : MonoBehaviour
 {
     public static Action OnTicketsChange;
-
+    public static Action OnItemBought;
     public static void ChangeTicket()
     {
         OnTicketsChange?.Invoke();
+    }
+
+    public static void ItemBought()
+    {
+        OnItemBought?.Invoke();
     }
 
 }
